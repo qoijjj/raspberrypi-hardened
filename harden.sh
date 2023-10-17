4337a1f290dbee10ddc6718baba88c886f8fd7fa
qoijjj <129108030+qoijjj@users.noreply.github.com>
2023-10-16 18:03:47 -0700

    Update harden.sh

--- harden.sh
+++ harden.sh
@@ -37,17 +37,4 @@ cp ./config/50-unattendedupgrades.conf /etc/apt/apt.conf.d/
 echo "Setting hardening kernel parameters..."
 echo " init_on_alloc=1 init_on_free=1 slab_nomerge page_alloc.shuffle=1 randomize_kstack_offset=on vsyscall=none debugfs=off lockdown=confidentiality random.trust_cpu=off random.trust_bootloader=off nvme_core.default_ps_max_latency_us=0 mitigations=auto,nosmt" >> /boot/cmdline.txt
 
-echo "Building hardened_malloc..."
-apt-get install -y make gcc g++
-rm -r hardened_malloc-12
-wget https://github.com/GrapheneOS/hardened_malloc/archive/refs/tags/12.tar.gz
-tar -xvf 12.tar.gz
-cd hardened_malloc-12
-make VARIANT=light
-
-echo "Installing hardened_malloc"
-cp out-light/libhardened_malloc-light.so /usr/local/lib/libhardened_malloc-light.so
-cd ..
-cp -f ./config/ld.so.preload /etc/
-
 echo "Complete. Reboot your system for changes to take effect."
